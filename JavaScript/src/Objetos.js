dd70eab4754b7382409aee03c9c7a40ebd69d94c
Mauricio Vargas <mauricio.vargas.t@gmail.com>
2023-06-30 18:52:22 -0400

    Agregando un archivo js que ejemplifica la declaración de objetos

---
+++ JavaScript/src/Objetos.js
@@ -0,0 +1,20 @@
+/* 
+    Los objetos son tipos de datos que agrupan varios tipos de datos, por ejemplo los objetos de los juegos.
+    en síntesis es una agrupación de datos
+*/
+
+//Ejemplo de objetos constantes
+const MyVoidObject = {} //Este es un objeto vacío, no posee ningún tipo de dato dentro de él.
+
+/* Objeto con atributos cada atributo debe de definirse con 2 puntos y cada final de atributo es con coma para
+referirse al siguiente atributo. */
+const MyObject = {
+    ANumber : 12,
+    AString : "esta es una cadena dentro de un objeto",
+    ACondition : true,
+}
+
+console.log(MyVoidObject);
+
+console.log(MyObject);
+
