97f316096414dbd17c2faf5b64b9c0032acb9c9c
Mauricio Vargas <mauricio.vargas.t@gmail.com>
2023-06-30 18:17:04 -0400

    Cambiando nombre y contenido del archivo ConsoleLog

--- JavaScript/src/consoleLog.js
+++ JavaScript/src/consoleLog.js
@@ -1,5 +1,7 @@
-// La console.log ("") Sirve para imprimir el texto entre parentesis por consola.
+/*
+    La console.log ("") Sirve para imprimir el texto entre parentesis por consola. 
+    Esto solo será visible si lo vemos en el navegador con el instpector.
+*/
 // Ejemplos:
 console.log('Hola mundo');
 console.log('Chao mundo');
-
