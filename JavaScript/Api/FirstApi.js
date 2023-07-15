029461317b75b854cef2ffa6de6fe3b718a2e8cc
Mauricio Vargas <mauricio.vargas.t@gmail.com>
2023-07-14 22:22:28 -0400

    Terminando con una pequeña aplicación

--- JavaScript/Api/FirstApi.js
+++ JavaScript/Api/FirstApi.js
@@ -10,17 +10,30 @@ app.use(express.json());
 
 mongoose.connect('mongodb+srv://Mauricio:m9BsCw9xu402bkAO@testcluster.38yokte.mongodb.net/MiApp?retryWrites=true&w=majority');
 
-app.get("/",user.list);
-app.post("/", user.create);
-app.get("/:id",user.get);
-app.put("/:id",user.update);
-app.patch("/:id",user.update);
-app.delete("/:id",user.destroy);
-//Al final de todas las rutas que no se encuentren
+
+
+app.get("/users",user.list);
+app.post("/users", user.create);
+app.get("/users/:id",user.get);
+app.put("/users/:id",user.update);
+app.patch("/users/:id",user.update);
+app.delete("/users/:id",user.destroy);
+
+
+app.use(express.static("APP")) //Servir archivos que están en la misma carpeta
+
+//Raiz
+app.get("/",(req,res) => {
+    console.log(__dirname);
+    res.sendFile(`${__dirname}/index.html`);
+})
+
 app.get("*", (req,res)=>
 {
     res.status(404).send("Esta página no existe");
 });
+
+//Al final de todas las rutas que no se encuentren
 app.post("*", (req,res)=>
 {
     res.status(404).send("Esta página no existe");
